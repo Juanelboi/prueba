b7db8efff0d0a35227e50dafe4a13410f761bb8b
John Doe <juangarciaboissier@gmail.com>
2024-10-05 12:29:31 +0100

    Preba 2

--- src/main/java/software/ulpgc/Main.java
+++ src/main/java/software/ulpgc/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Person Juan = new Person("Juan", LocalDate.of(2004,05,3));
         System.out.println(Juan);
+        System.out.println("Skibidi");
     }
 
 }
